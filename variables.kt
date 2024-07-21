aec259f27fbac31110f4ba52e3fa67684665da94
Shreya-1206 <shreyaray.1206@gmail.com>
2024-07-21 13:00:15 +0530

    first commit

---
+++ variables.kt
@@ -0,0 +1,11 @@
+// var and val
+fun main() {
+    var name = "shreya"
+    val hobby = "dancing"
+    name = "Digvijay"
+    // hobby = "Acting"   /// here val cannot be reAssigned becuse it is strict than var 
+    println("Name : " + name)
+    println("Hobby : " + hobby)
+
+}
+
