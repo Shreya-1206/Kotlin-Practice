76e4832f68539ea434dd0f3f4225abbbf7af2028
Shreya-1206 <shreyaray.1206@gmail.com>
2024-07-22 17:49:40 +0530

    fifth commit

--- numberConversion.kt
+++ numberConversion.kt
@@ -1,11 +1,15 @@
 fun main() {
-    //  var a : Int = 1
-    //  val convert_long : Long? = a 
-    //  println(a == convert_long)    //Error identity equality for arguments types byte and int 
+    //var a : Int = 1
+    //val convert_long : Long? = a 
+    //println(a === convert_long)   
+     //Error intailizer mismatch excepted long?
     
     val b : Byte = 100
     val bl : Int? = b?.toInt()
     println(bl)
-    print(bl === b?.toInt())
+    //print(b === bl)  error says prohibited
+    println(bl === b?.toInt()) 
 
 }
+
+//doubt is that first example did'nt converted at all but the second did with an error 
---
+++ operatorsOnNumbers.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var div = 5/2
+    //println(div == 2.5f)  // error : equality cannot be applied to int andd double or float
+    println(div == 2)
+
+    val x = 5L / 2
+    println(x == 2.7L)
+}
