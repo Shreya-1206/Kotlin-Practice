9befa2156760285d802463dd1fa0180ad4327ee5
Shreya-1206 <shreyaray.1206@gmail.com>
2024-07-22 12:51:41 +0530

    second commit

---
+++ DataTypesNumbers.kt
@@ -0,0 +1,26 @@
+val top_level_declaration : Int = 1234
+
+
+fun main() {
+    // All data Types
+    val myNum = 5             // Int
+    val myDoubleNum = 5.99    // Double
+    val myLetter = 'D'        // Char
+    val myBoolean = true      // Boolean
+    val myText = "Hello"      // String
+
+
+    //Numbers divided into  -->> Integers positive or negative (such as 123 or -456) and       floating (float and Double)
+          //Integers - Byte, Short , Int, Long
+            //BYTE data type can store whole numbers from -128 to 127
+
+            val num_byte : Byte = 120
+            println("Byte Number : " + num_byte)
+            println("We can assces the top level declaration like this - " + top_level_declaration)
+
+            //The SHORT data type can store whole numbers from -32768 to 32767
+            val num_short = -23234
+            println("Short Number : " + num_short)
+
+
+}
--- variables.kt
+++ variables.kt
@@ -2,10 +2,28 @@
 fun main() {
     var name = "shreya"
     val hobby = "dancing"
+    val _isAdmin = false
+
+    // However we can also specify type 
+    var name2 : String = "Hinata"
+    val hobby2 : String = "Drawing"
+    var age : Int = 24
+    
+    //We can also declare variables without assiging values
+    var name3 : String
+    val ageKiya : Int
+    ageKiya = 24
+    name3 = "Kiya"
     name = "Digvijay"
-    // hobby = "Acting"   /// here val cannot be reAssigned becuse it is strict than var 
+ // hobby = "Acting"   /// here val cannot be reAssigned becuse it is strict than var 
+    
     println("Name : " + name)
+    println("Shreya is Admin : " + _isAdmin)
     println("Hobby : " + hobby)
+    println("Hobby Of Hinata :" + hobby2)
+    println("Age of Kiya : " + ageKiya)
 
 }
+
+
 
